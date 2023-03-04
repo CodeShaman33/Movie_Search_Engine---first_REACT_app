8121005ef6a20c75929825acf896b742e3335780
CodeShaman33 <mar.donczyk@gmail.com>
2023-03-04 19:43:56 +0100

    add map function that creates movies divs and another state for search term

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./Card";
 
 const API_URL = "https://www.omdbapi.com?apikey=20849a0e";
 
-const movie1 = {
-  Title: "Hope Springs Eternal: A Look Back at 'The Shawshank Redemption'",
-  Year: "2004",
-  imdbID: "tt0443041",
-  Type: "movie",
-  Poster: "N/A",
-};
-
-
-
 const App = () => {
+  //STATES (there is no limit of states  in component)
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data);
+    setMovies(data.Search);
   };
 
   useEffect(() => {
-    searchMovies("The Shawshank Redemption");
+    searchMovies("SEX");
   }, []);
 
   return (
     <div className="App">
-        <h1>Movies We Love</h1>
+      <h1>Movies We Love</h1>
 
-        <div className="search">
-          <input type="text"
-          placeholder="search movie "
-           />
-           <button>Search</button>
-        </div>
+      <div className="search">
+        <input type="text"
+         placeholder="search movie "
+         value={searchTerm}
+         onChange = {(e) => setSearchTerm(e.target.value)} />
+        <button onClick={() => searchMovies(searchTerm)}>Search</button>
+      </div>
 
-        <div className="container" >
-          <Card movie={movie1}/>
-        </div>
+      <div className="container">
+        {movies?.length > 0 ? (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Card movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>no movies found</h2>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
